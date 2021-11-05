011a1b8b882f50b5c48bc54d13e8aa1a2a2bc084
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-05 23:57:09 +0530

    Adding Peek And Pop Method To Stack Using LinkedList

--- src/com/bridgelabz/StackAndQueues.java
+++ src/com/bridgelabz/StackAndQueues.java
@@ -2,19 +2,27 @@ package com.bridgelabz;
 
 public class StackAndQueues {
  public static void main(String args[]){
-        /* stack implement use linkedlist
-            create a generic method and class to give any data
-         */
+    /* Queue implement use linkedlist
+     * create a generic method and class to give any data
+     */
     System.out.println("Welcome to stack implemetation use linkedlist");
-    //create object and give the push method value
-    StackUsingLinkedList stackUsingLinkedList = new StackUsingLinkedList();
-    stackUsingLinkedList.push(56);
-    stackUsingLinkedList.push(30);
-    stackUsingLinkedList.push(70);
-    stackUsingLinkedList.display();
-    System.out.println("\ntop value"+stackUsingLinkedList.peek());
-    stackUsingLinkedList.pop();
-    stackUsingLinkedList.pop();
-    System.out.println("top value"+stackUsingLinkedList.peek());
+    QueueUsingLinkedList queuqUsingLinkedList = new QueueUsingLinkedList();
+    queuqUsingLinkedList.enqueue(56);
+    queuqUsingLinkedList.enqueue(30);
+    queuqUsingLinkedList.enqueue(60);
+    queuqUsingLinkedList.display();
+    System.out.println("queue front"+queuqUsingLinkedList.front.key);
+
+
+        /*create object and give the push method value
+        StackUsingLinkedList stackUsingLinkedList = new StackUsingLinkedList();
+        stackUsingLinkedList.push(56);
+        stackUsingLinkedList.push(30);
+        stackUsingLinkedList.push(70);
+        stackUsingLinkedList.display();
+        System.out.println("\ntop value"+stackUsingLinkedList.peek());
+        stackUsingLinkedList.pop();
+        stackUsingLinkedList.pop();
+        System.out.println("top value"+stackUsingLinkedList.peek());*/
  }
 }
